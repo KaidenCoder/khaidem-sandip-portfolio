c4c37baabfe30c6222f470aa5b89407e6a4564f4
Khaidem Sandip Singha <khaidem@Khaidems-MacBook-Air.local>
2025-07-20 22:59:24 +0530

    correct linkedin url

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
                 <p>
                     If you’d like to get in touch, feel free to reach out via email at 
                     <a href="mailto:kss@gmail.com" className="contact-link"> here</a> or connect with me on 
-                    <a href="https://www.linkedin.com/in/khaidem-sandip/" target="_blank" rel="noopener noreferrer" className="contact-link"> LinkedIn</a>.
+                    <a href="https://www.linkedin.com/in/khaidemsandip/" target="_blank" rel="noopener noreferrer" className="contact-link"> LinkedIn</a>.
                 </p>
                 <p>Looking forward to hearing from you!</p>
             </div>
